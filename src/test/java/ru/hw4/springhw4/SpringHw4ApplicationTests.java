c219dbf64063f58ce96cff3caa74312d34cec0b6
Evgeniy <malugn-evg-nsk@yandex.ru>
2022-04-03 16:55:08 +0700

    hw4

---
+++ src/test/java/ru/hw4/springhw4/SpringHw4ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.hw4.springhw4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringHw4ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
